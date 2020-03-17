a82e9cced4bc1dc7668efed9426decb3221a769f
Artur <luart004@gmail.com>
2020-03-17 10:55:18 +0500

    Задачи 3, 4, 5

--- example.py
+++ example.py
@@ -1,5 +1,5 @@
-#Задачи на циклы и оператор условия------
-#----------------------------------------
+# Задачи на циклы и оператор условия------
+# ----------------------------------------
 
 '''
 Задача 1
@@ -15,49 +15,48 @@ for i in range(1, 6):
 '''
 i, match_quantity = 0, 0
 
-while i < 10:
-    if int(input('Введите число ')) == 5:
-        match_quantity += 1
-    i += 1
-print('Количество цифр 5: ', match_quantity)
+# while i < 10:
+#     if int(input('Введите число ')) == 5:
+#         match_quantity += 1
+#     i += 1
+# print('Количество цифр 5: ', match_quantity)
 
 '''
 Задача 3
 
 Найти сумму ряда чисел от 1 до 100. Полученный результат вывести на экран.
 '''
-# sum = 0
-#
-# for i in range(1,101):
-#     sum+=i
-# print(sum)
+sum = 0
+
+for i in range(1, 101):
+    sum += i
+print(sum)
 
 '''
 Задача 4
 
 Найти произведение ряда чисел от 1 до 10. Полученный результат вывести на экран.
 '''
+op = 1
+for i in range(1, 11):
+    op *= i
+print(op)
 
 '''
 Задача 5
 
 Вывести цифры числа на каждой строчке.
 '''
-
-# integer_number = 2129
-#
-# #print(integer_number%10,integer_number//10)
-#
-# while integer_number>0:
-#     print(integer_number%10)
-#     integer_number = integer_number//10
+integer_number = 2129
+while integer_number > 0:
+    print(integer_number % 10)
+    integer_number = integer_number // 10
 
 '''
 Задача 6
 
 Найти сумму цифр числа.
 '''
-
 
 '''
 Задача 7
@@ -71,19 +70,19 @@ print('Количество цифр 5: ', match_quantity)
 Дать ответ на вопрос: есть ли среди цифр числа 5?
 '''
 integer_number = 213413
-while integer_number>0:
-    if integer_number%10 == 5:
+while integer_number > 0:
+    if integer_number % 10 == 5:
         print('Yes')
         break
-    integer_number = integer_number//10
-else: print('No')
+    integer_number = integer_number // 10
+else:
+    print('No')
 
 '''
 Задача 9
 
 Найти максимальную цифру в числе
 '''
-
 
 '''
 Задача 10
